341644fa7e053ded77935b913b531d94ccb1a40a
aneme1038 <thriftman25@gmail.com>
2019-08-01 18:58:18 -0400

    test2

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const methodOverride  = require('method-override');
 const mongoose = require ('mongoose');
 const app = express ();
 const db = mongoose.connection;
-const model = require('./models');
 //___________________
 //Port
 //___________________
